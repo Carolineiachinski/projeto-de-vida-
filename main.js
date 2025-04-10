086553501edc1ea174d92f64c198a9e7676250a2
Carolineiachinski <146885943+Carolineiachinski@users.noreply.github.com>
2025-04-10 10:22:51 -0300

    site

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = document-querySelectorA11(".botao");
+for (let i = 0; i <bootoes.length; i++) { 
+    botoes{i}.onclick = function () {
+        for ( j = j <botoes.leng j++) {
+            botoes[j].classList.remove("ativo"); 
+            const element = ar j];
+            
+        }
+        
+    }
+    botoes[i] .classList.add("ativo")
+}; 
+} 
